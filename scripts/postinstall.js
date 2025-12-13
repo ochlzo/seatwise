f8557758adcc69004fb9cda8a87b5b97f0f38033
ochlzo <cholocandelaria123@gmail.com>
2025-12-13 17:39:40 +0800

    removed prisma dependencies

--- scripts/postinstall.js
+++
@@ -1,8 +0,0 @@
-const { execSync } = require("child_process");
-
-if (process.env.RUN_PRISMA_GENERATE === "true") {
-  execSync("npx prisma generate", { stdio: "inherit" });
-} else {
-  console.log("Skipping prisma generate (set RUN_PRISMA_GENERATE=true to enable)");
-}
-
