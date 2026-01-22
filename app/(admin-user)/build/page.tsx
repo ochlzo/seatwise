0e9169af877914a5371bdb18d29595f211bf1939
ochlzo <cholocandelaria123@gmail.com>
2026-01-22 22:57:11 +0800

    added toolbar

--- app/(admin-user)/build/page.tsx
+++ app/(admin-user)/build/page.tsx
@@ -3,7 +3,8 @@
 
 import React from "react";
 import dynamic from "next/dynamic";
-import { Toolbar, SelectionPanel } from "@/components/seatmap/UIOverlays";
+import { Toolbar as ModeToolbar, SelectionPanel } from "@/components/seatmap/UIOverlays";
+import SeatmapToolbar from "@/components/seatmap/toolbar";
 import { SeatMapSidebar } from "@/components/seatmap/seatmap-sidebar";
 import { SeatmapPageHeader } from "@/components/seatmap/seatmap-page-header";
 import { SidebarInset, SidebarProvider } from "@/components/ui/sidebar";
@@ -29,7 +30,8 @@ export default function TestPage() {
                     rightSlot={<span className="text-xs text-zinc-500">app/test/page.tsx</span>}
                 />
                 <div className="flex-1 relative bg-zinc-100 dark:bg-zinc-900 overflow-hidden">
-                    <Toolbar />
+                    <ModeToolbar />
+                    <SeatmapToolbar />
                     <SelectionPanel />
                     <SeatmapCanvas />
                 </div>
